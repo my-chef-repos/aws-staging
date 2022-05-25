03d6928f7340b8fedf14644dfebe534196c01139
r-goto <r-goto@myMac-16.local>
2022-05-25 13:09:26 +0900

    ssh_setup v1.0.0

--- cookbooks/ssh_setup/attributes/default.rb
+++ cookbooks/ssh_setup/attributes/default.rb
@@ -1,2 +1,5 @@
-# `setup_secure_ssh_config` or `setup_insecure_ssh_config`
-default['ssh_setup']['ssh_config'] = 'setup_secure_ssh_config'
+# v1.0.0 for Insecure settings
+default['ssh_setup']['ssh_config'] = 'setup_insecure_ssh_config'
+
+# v2.0.0 for Secure settings
+# default['ssh_setup']['ssh_config'] = 'setup_secure_ssh_config'
